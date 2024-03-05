95d804de7a478e6368a20303cf303382e64a9418
Mohamed Chakhman <medchakhman@gmail.com>
2024-03-05 22:45:46 +0100

    code: all repo

--- sort.h
+++ sort.h
@@ -22,15 +22,15 @@ typedef struct list_n
 
 /** functions **/
 void insertion_sort_list(list_t **list);
-void bubble_sort(int *arr, size_t size_n);
-void selection_sort(int *arr, size_t size_n);
-void print_array(const int *arr, size_t size_n);
-void shell_sort(int *arr, size_t size_n);
-size_t partition(int *arr, size_t low, size_t high, size_t size_n);
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
+size_t partition(int *array, size_t low, size_t high, size_t size);
 void print_list(const list_t *list);
 void swap(int *a, int *b);
-void quick_sort(int *arr, size_t size_n);
-void quick_sort_recursion(int *arr, size_t low, size_t high, size_t size_n);
-void counting_sort(int *arr, size_t size_n);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursion(int *array, size_t low, size_t high, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
